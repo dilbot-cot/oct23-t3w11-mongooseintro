23be432200a7c9f3cb85ee9c2ad4a55525fb274d
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-06-18 21:02:39 +1000

    feat: added POST method

--- src/controllers/ContactRouter.js
+++ src/controllers/ContactRouter.js
@@ -21,13 +21,27 @@ router.get("/all", async (request, response) => {
 })
 
 // GET localhost:3000/contacts/:id
-router.get("/:id", (request, response) => {
+router.get("/:id", async (request, response) => {
+    let results = await Contact.findById(request.params.id).exec();
+    console.log("Found Documents: ")
+    console.log(results)
 
+    response.json({
+        message: "Found documents!",
+        data: results
+    })
 })
 
 // POST localhost:3000/contacts/
-router.post("/", (request, response) => {
+router.post("/", async (request, response) => {
+    let results = await Contact.create(request.body);
+    console.log("Created Documents: ")
+    console.log(results)
 
+    response.json({
+        message: "Created documents!",
+        data: results
+    })
 })
 
 // PATCH localhost:3000/contacts/:id
